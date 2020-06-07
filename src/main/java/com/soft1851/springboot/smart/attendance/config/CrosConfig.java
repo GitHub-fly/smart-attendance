da3855dd370ee3163cbc5996a4ff3664bdd4c0f7
wangDk199 <1091886414@qq.com>
2020-06-07 09:24:35 +0800

    项目搭建

---
+++ src/main/java/com/soft1851/springboot/smart/attendance/config/CrosConfig.java
@@ -0,0 +1,25 @@
+package com.soft1851.springboot.smart.attendance.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author : 顾兆远
+ * @program: client-api
+ * @description: 解决跨越问题
+ * @date : 2020-06-01 10:14
+ **/
+@Configuration
+public class CrosConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
