2f758fda48e4377c813dc0e203710b4a7c67db13
wangDk199 <1091886414@qq.com>
2020-06-08 17:27:55 +0800

    用户实体类

---
+++ src/main/java/com/soft1851/springboot/smart/attendance/model/entity/SysUser.java
@@ -0,0 +1,161 @@
+package com.soft1851.springboot.smart.attendance.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @author ke
+ * @ClassName SysUser
+ * @Description TOOD
+ * @Date 2020/6/8
+ * @Version 1.0
+ **/
+@Entity
+@Data
+public class SysUser {
+    /**
+     * 用户id
+     */
+    @Id
+    @Column(name = "pk_sys_user_id", length = 32)
+    @NotNull
+    private String pkSysUserId;
+
+    /**
+     * 用户真实名字
+     */
+    @NotNull
+    @Column(name = "sys_user_name", length = 60)
+    private String sysUserName;
+
+    /**
+     * 用户昵称
+     */
+    @NotNull
+    @Column(name = "sys_user_nickname", length = 60)
+    private String sysUserNickname;
+
+    /**
+     * 性别
+     */
+    @NotNull
+    @Column(name = "sys_user_gender", length = 10)
+    private String sysUserGender;
+
+    /**
+     * 学号、教工号、职工号
+     */
+    @NotNull
+    @Column(name = "sys_job_number", length = 60)
+    private String sysJobNumber;
+
+    /**
+     * 阿姨所管楼层
+     */
+    @NotNull
+    @Column(name = "sys_room", length = 60)
+    private String sysRoom;
+
+    /**
+     * 学生所属房间
+     */
+    @NotNull
+    @Column(name = "room_number", length = 60)
+    private String roomNumber;
+
+    /**
+     * 班主任名称
+     */
+    @NotNull
+    @Column(name = "sys_user_teacher_name", length = 60)
+    private String sysUserTeacherName;
+
+    /**
+     * 学生所属班级或班主任管理班级
+     */
+    @NotNull
+    @Column(name = "sys_class_name", length = 60)
+    private String sysClassName;
+
+    /**
+     * 学生所属院系
+     */
+    @NotNull
+    @Column(name = "sys_user_academy", length = 60)
+    private String sysUserAcademy;
+
+    /**
+     * 学生今日打卡
+     */
+    @NotNull
+    @Column(name = "is_attendance", length = 0)
+    private Integer isAttendance;
+
+    /**
+     * 用户密码
+     */
+    @NotNull
+    @Column(name = "sys_user_password", length = 32)
+    private String sysUserPassword;
+
+    /**
+     * 手机号
+     */
+    @NotNull
+    @Column(name = "sys_user_phone", length = 30)
+    private String sysUserPhone;
+
+    /**
+     * 头像
+     */
+    @NotNull
+    @Column(name = "sys_user_avatar", length = 254)
+    private String sysUserAvatar;
+
+    /**
+     * 所属角色id
+     */
+    @NotNull
+    @Column(name = "role_id", length = 11)
+    private Integer RoleId;
+
+    /**
+     * 加盐
+     */
+    @NotNull
+    @Column(name = "salt", length = 4)
+    private String salt;
+
+    /**
+     * 账户状态
+     */
+    @NotNull
+    @Column(name = "status", length = 4)
+    private Integer status;
+
+    /**
+     * 更新时间
+     */
+    @NotNull
+    @Column(name = "gmt_modified", length = 0)
+    private Date gmtModified;
+
+    /**
+     * 删除标志
+     */
+    @NotNull
+    @Column(name = "delete_flag", length = 4)
+    private Integer deleteFlag;
+
+    /**
+     * 创建时间
+     */
+    @NotNull
+    @Column(name = "gmt_create", length = 0)
+    private Date gmtCreate;
+}
