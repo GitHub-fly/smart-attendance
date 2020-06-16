93a854493ecc2e2fcaf2b364ab3f6fcc806dc03b
czzeng-code <czzeng_ok@qq.com>
2020-06-16 13:43:20 +0800

    api加密

---
+++ src/main/java/com/soft1851/springboot/smart/attendance/config/FilterConfig.java
@@ -0,0 +1,11 @@
+package com.soft1851.springboot.smart.attendance.config;
+
+/**
+ * @author Zeng
+ * @ClassName FilterConfig
+ * @Description TOOD
+ * @Date 2020/6/16
+ * @Version 1.0
+ **/
+public class FilterConfig {
+}
