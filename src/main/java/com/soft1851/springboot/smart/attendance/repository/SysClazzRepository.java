11f7be4022845812ea579a0a20bd351fab10e2f2
wangDk199 <1091886414@qq.com>
2020-06-18 13:59:37 +0800

    解决冲突

--- src/main/java/com/soft1851/springboot/smart/attendance/repository/SysClazzRepository.java
+++ src/main/java/com/soft1851/springboot/smart/attendance/repository/SysClazzRepository.java
@@ -1,10 +1,7 @@
 package com.soft1851.springboot.smart.attendance.repository;
 
 import com.soft1851.springboot.smart.attendance.model.entity.SysClazz;
-<<<<<<< HEAD
 import com.soft1851.springboot.smart.attendance.model.vo.TeacherVo;
-=======
->>>>>>> 22d86ecccc883f4da8a6c7c8eef8c22cab7ce30b
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,7 +9,6 @@ import java.util.List;
 
 /**
  * @author ke
-<<<<<<< HEAD
  * @ClassName SysClazzRepoditory
  * @Description TOOD
  * @Date 2020/6/18
@@ -31,13 +27,6 @@ import java.util.List;
             + "ON u.pkSysUserId = i.teacherId "
             + "WHERE i.instructorId = ?1")
     List<TeacherVo> findTeacherVoByInstructorId(String instructorId);
-=======
- * @ClassName SysClazzRepository
- * @Description TOOD
- * @Date 2020/6/17
- * @Version 1.0
- **/
-public interface SysClazzRepository extends JpaRepository<SysClazz, Integer> {
 
     /**
      * 根据辅导员id和姓名查询所管理的班级
@@ -46,5 +35,4 @@ public interface SysClazzRepository extends JpaRepository<SysClazz, Integer> {
             "ON c.academyName = u.sysUserInstructorName " +
             "WHERE u.pkSysUserId = ?1 AND c.academyName = ?2 ")
     List<SysClazz> findAllByAcademyNameAndPkSysUserId(String userId, String academyName);
->>>>>>> 22d86ecccc883f4da8a6c7c8eef8c22cab7ce30b
 }
